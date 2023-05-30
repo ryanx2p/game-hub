8163bbba47bf8b65866b8d4e7c19288d518e3020
rye <ryanx2p@gmail.com>
2023-05-30 18:03:57 +0800

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
     id: number;
     name: string;
     background_image: string;
-    parent_platform: {platform: Platform}[]
+    parent_platform: {platform: Platform}[];
+    metacritic: number;
   }
   
   interface FetchGamesResponse {
