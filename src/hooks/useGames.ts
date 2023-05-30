a77b6432dce60125dedb0fefe94d7210eb94efb0
rye <ryanx2p@gmail.com>
2023-05-30 17:50:27 +0800

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
+    background_image: string;
+    parent_platform: {platform: Platform}[]
   }
   
   interface FetchGamesResponse {
@@ -23,11 +31,11 @@ const useGames= () => {
           .get<FetchGamesResponse>("/games", {signal: controller.signal})
           .then((res) => setGames(res.data.results))
           .catch((err) => {
-            if(err instanceof CanceledError ) return;
+            if (err instanceof CanceledError ) return;
             setError(err.message)
             });
 
-          return () => controller.abort();
+        return () => controller.abort();
       }, []);
 
       return {games, error};
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '5a71d1ce3016408c95517df5adcf9e5a'
+        key: '2f68c64fc85a4182b05f3f6c35aefd08'
     }
 })
