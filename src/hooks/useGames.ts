cfa873b588b879f72f10056e385f90df373519d0
rye <ryanx2p@gmail.com>
2023-06-01 13:17:26 +0800

    Replaced parent_platform to parent_platforms to match with the API

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,7 +11,7 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platform: { platform: Platform }[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
 }
